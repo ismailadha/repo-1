6f86efa30dca2d0330bb9b9effcc53948b1c2010
ismailadha <ismailadha06@gmail.com>
2021-05-29 13:19:27 +0700

    update lagi

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-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
-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
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
